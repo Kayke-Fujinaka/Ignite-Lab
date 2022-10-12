cf63377eb129bc7d97010e329edf68d369043054
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-10-12 18:08:50 -0300

    feat: TextInput component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,17 +12,20 @@ module.exports = {
         "2xl": 32,
       },
       colors: {
+        transparent: "transparent",
         white: "#FFFFFF",
         black: "#000000",
-
-        "gray-900": "#121214",
-        "gray-800": "#202024",
-        "gray-400": "#7C7C8A",
-        "gray-200": "#C4C4CC",
-        "gray-100": "#E1E1E6",
-
-        "cyan-500": "#81D8F7",
-        "cyan-300": "#A1E5FE",
+        gray: {
+          900: "#121214",
+          800: "#202024",
+          400: "#7C7C8A",
+          200: "#C4C4CC",
+          100: "#E1E1E6",
+        },
+        cyan: {
+          500: "#81D8F7",
+          300: "#A1E5FE",
+        },
       },
       fontFamily: {
         sans: "Inter, sans-serif",
