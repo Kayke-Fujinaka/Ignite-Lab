aca1e992c41e987a348264c7a587318ea8373870
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-10-14 09:32:18 -0300

    story: SignIn page

---
+++ src/pages/SignIn.stories.tsx
@@ -0,0 +1,33 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { within, userEvent, waitFor } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
+import { SignIn } from "./SignIn";
+
+export default {
+  title: "Components/SignIn",
+  component: SignIn,
+  args: {},
+  argTypes: {},
+} as Meta;
+
+export const Default: StoryObj = {
+  play: ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    userEvent.type(
+      canvas.getByPlaceholderText("Digite seu e-mail"),
+      "kaykealvesfujinaka@gmail.com"
+    );
+    userEvent.type(canvas.getByPlaceholderText("******"), "12345678");
+
+    userEvent.click(canvas.getByRole("button"));
+
+    waitFor(() => {
+      expect(
+        canvas.getByText(
+          "Login Realizado!"
+        )
+      ).toBeInTheDocument();
+    })
+  },
+};
